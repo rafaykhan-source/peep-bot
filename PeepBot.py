3131457d17eeeb511c35a36af77ddc449101e8a6
Rafay Khan <82721722+rafaykhan-source@users.noreply.github.com>
2022-08-06 14:47:22 -0400

    Update PeepBot.py

--- PeepBot.py
+++ PeepBot.py
@@ -5,10 +5,14 @@ import discord
 
 def configure():
   load_dotenv()
+  
+# helper method
+def verify_admin(messager):
+  if 'Admin' not in str(messager.roles):
+      raise Exception("Messager must be an Admin")
 
 # reading in sample role spreadsheet
 df = pd.read_csv('sample.csv')
-print(df)
 
 # establishing intents
 intents = discord.Intents.all()
@@ -24,76 +28,70 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-  server = message.author.guild
-  # ensuring that the messager isn't the bot itself
+  # ensures that the messager isn't the bot itself
   if message.author == client.user:
     return
-    
-  # peep responds to messages containing peep
+  
+  # responds 'peep!' to messages containing peep
   if 'peep' in message.content:
     await message.channel.send("peep!")
     print(message.content)
     return
   
-  # person verification command
-  # !missing : spreadsheet_role_name
+  # retrieves discord server 
+  server = message.author.guild
+  
+  """ 
+  identifies missing server members based on spreadsheet
+  !missing : spreadsheet_column_name
+  """
   if message.content.startswith('!missing'):
-    # checking if messager is a server admin
-    if 'Admin' not in str(message.author.roles):
-      return
+    # checking if messager is an admin
+    verify_admin(message.author)
+    # parsing command arguments
     input = message.content.split(' : ') 
-    people = df[input[1]]
-    members = server.members
-    
-    found = []
+    # storing spreadsheet people
+    people = list(df[input[1]])
+    # identifying missing server members
     for person in people:
-      for m in members:
-        if person in m.display_name: 
-          found.append(person)
-          break
-    
-    for person in people:
-      if person not in found:
-        await message.channel.send(f"{person} not found")
+      m = discord.utils.find(lambda m : person in m.display_name, server.members)
+      if m == None:
+        await message.channel.send(f"could NOT find: {person}") 
     return
   
-  # autoassign roles command
+  ''' 
+  autoassigns roles based on spreadsheet
   # !autoassign : spreadsheet_role_name : discord_server_role_name
+  '''
   if message.content.startswith('!autoassign'):
     # checking if messager is a server admin
-    if 'Admin' not in str(message.author.roles):
-      return
-    # converting input into a list of arguments
+    verify_admin(message.author)
+    # parsing command arguments
     input = message.content.split(" : ")
     # validating role argument
     role = discord.utils.get(server.roles, name=input[2])
-    print(role)
     if role == None:
       await message.channel.send("no such discord role exists")
       return
     # validating spreadsheet column
     try:
-      people = df[input[1]]
+      people = list(df[input[1]])
     except:
       await message.channel.send("no such spreadsheet column exists")
       return
     # storing server members and iterating over them
-    members = server.members 
-    for m in members:
-      name = str(m.display_name)
+    for person in people:
+      m = discord.utils.find(lambda m : person in m.display_name, server.members)
+      if m == None: 
+        await message.channel.send(f'could NOT find: {person}')
+        continue
       if role in m.roles:
-        await message.channel.send(f"{name} already has role {role}")
-      elif name in list(people):
-        await m.add_roles(role)
-        if role in m.roles:
-          await message.channel.send(f"assigned {role} to {name}")
-        else:
-          await message.channel.send(f"failed to assign {role} to {name}")
+        await message.channel.send(f"{person} ALREADY has role {role}")
       else:
-        await message.channel.send(f"{name} was not been assigned {role}.")
+        await m.add_roles(role)
+        await message.channel.send(f"ASSIGNED {role} to {person}")
     await message.channel.send("Finished.")
     return
   
 configure()
 client.run(os.getenv('peep'))
-
